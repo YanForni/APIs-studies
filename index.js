f97cdc1d589e7fd723e0ebaf84b2b215b7669caa
Yan <yanfornimedeiros@gmail.com>
2022-08-10 15:30:10 -0300

    Entendendo um pouco sobre GET, POST, PUT, DELETE

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+app.listen('3000')
+
+
+//método get
+// app.route('/').get( (req,res)=> res.send("hello") )
+// app.route('/sobre').get( (req,res)=> res.send("hello sobre") )
+
+
+//método post
+// app.use(express.json())
+// app.route('/').post( (req,res) => res.send(req.body) )
+
+
+//método put
+// app.use(express.json())
+// let author = "Nay"
+// app.route('/').get( (req,res)=> res.send(author) )
+// app.route('/').put( (req,res) => {
+//     author = req.body
+// })
+//     res.send(author)
+
+
+//método delete
+
+let author = "Yan"
+
+app.route('/:identifier').delete( (req, res) => {
+    res.send(req.params.identifier)
+} )
