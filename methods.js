57ad7cc3c40a5d314b524412fde11458a4799ce4
Yan <yanfornimedeiros@gmail.com>
2022-08-10 15:42:00 -0300

    Alterando nome de arquivo

--- methods.js
+++ methods.js
@@ -6,16 +6,19 @@ app.listen('3000')
 
 
 //método get
+
 // app.route('/').get( (req,res)=> res.send("hello") )
 // app.route('/sobre').get( (req,res)=> res.send("hello sobre") )
 
 
 //método post
+
 // app.use(express.json())
 // app.route('/').post( (req,res) => res.send(req.body) )
 
 
 //método put
+
 // app.use(express.json())
 // let author = "Nay"
 // app.route('/').get( (req,res)=> res.send(author) )
@@ -28,7 +31,6 @@ app.listen('3000')
 //método delete
 
 let author = "Yan"
-
 app.route('/:identifier').delete( (req, res) => {
     res.send(req.params.identifier)
 } )
